7cf5ed4130d93f5a4b1c457051e0aeb5c0b22fd3
kehyshow <dante08290730@gmail.com>
2022-10-24 16:22:10 -0400

    update api

--- api/src/index.js
+++ api/src/index.js
@@ -1,5 +1,5 @@
-const cors = require("cors");
 const express = require("express");
+const cors = require("cors");
 const config = require('./config');
 const mail = require('./services/mail');
 
@@ -7,12 +7,22 @@ const app = express();
 app.use(express.json());
 app.use(cors());
 
-app.get('/sendMail', (req, res) => {
-  mail.sendMail(
-    'dante08290730@gmail.com',
-    'subject',
-    'Hello World!',
-  );
+app.post('/sendMail', async (req, res) => {
+  let status;
+  try {
+    // const { fromName, fromEmail, toName, toEmail, subject, body } = req.body;
+    console.log(req.body);
+    // await mail.sendMail(
+    //   'ironheart0225@gmail.com',
+    //   'subject',
+    //   'Hello World!',
+    // );
+    status = "success";
+  } catch(error) {
+    console.log('error:', error);
+    status = "failture";
+  }
+  res.json({ status });
 });
 
 app.listen(config.port, () => {
